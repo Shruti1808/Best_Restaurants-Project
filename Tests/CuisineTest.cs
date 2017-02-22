698feea66f552417221e001a11cf419c66c5d12e
Alexandra Holcombe, Renee Mei and Shruti Priya <alexandra+renee+shruti@>
2017-02-22 09:19:57 -0800

    add test for empty list at first

---
+++ Tests/CuisineTest.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BestRestaurants
+{
+  public class CuisineTest: IDisposable
+  {
+    public CuisineTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=best_restaurants_test;Integrated Security=SSPI;"
+    }
+
+    public void Dispose()
+    {
+      Cuisine.DeleteAll();
+    }
+
+    [Fact]
+    public void Test_CuisineTableEmptyAtFirst()
+    {
+      //Arrange Act
+      int result = Cuisine.GetAll().Count;
+
+      //Assert
+      Assert.Equal(0,result);
+    }
+  }
+}

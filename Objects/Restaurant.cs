e072fdbf6af2e476acca0df33c5590388646d031
Alexandra Holcombe and Renee Mei <alexandra+renee@>
2017-02-23 13:23:30 -0800

    Add open and close time to restaurants class

--- Objects/Restaurant.cs
+++ Objects/Restaurant.cs
@@ -11,13 +11,18 @@ namespace BestRestaurants
     private string _restaurantName;
     private int _cuisineId;
     private string _address;
+    private string _openTime;
+    private string _closeTime;
 
-    public Restaurant(string restaurantName, int cuisineId, string address, int Id = 0)
+    public Restaurant(string restaurantName, int cuisineId, string address, string openTime, string closeTime, int Id = 0)
     {
       _id = Id;
       _restaurantName = restaurantName;
       _address = address;
       _cuisineId = cuisineId;
+      _openTime = openTime;
+      _closeTime = closeTime;
+
     }
 
     public int GetId()
@@ -40,6 +45,16 @@ namespace BestRestaurants
       return _address;
     }
 
+    public string GetOpenTime()
+    {
+      return _openTime;
+    }
+
+    public string GetCloseTime()
+    {
+      return _closeTime;
+    }
+
     public override bool Equals(System.Object otherRestaurant)
     {
       if(!(otherRestaurant is Restaurant))
@@ -53,8 +68,10 @@ namespace BestRestaurants
         bool nameEquality = (this.GetRestaurantName() == newRestaurant.GetRestaurantName());
         bool cuisineIdEquality = (this.GetCuisineId() == newRestaurant.GetCuisineId());
         bool addressEquality = (this.GetAddress() == newRestaurant.GetAddress());
+        bool openTimeEquality = (this.GetOpenTime() == newRestaurant.GetOpenTime());
+        bool closeTimeEquality = (this.GetCloseTime() == newRestaurant.GetCloseTime());
 
-        return (idEquality && nameEquality && cuisineIdEquality && addressEquality);
+        return (idEquality && nameEquality && cuisineIdEquality && addressEquality && openTimeEquality && closeTimeEquality);
       }
     }
 
@@ -88,8 +105,10 @@ namespace BestRestaurants
         string restaurantName = rdr.GetString(1);
         int cuisineId = rdr.GetInt32(2);
         string address = rdr.GetString(3);
+        string openTime = rdr.GetString(4);
+        string closeTime = rdr.GetString(5);
 
-        Restaurant newRestaurant = new Restaurant(restaurantName, cuisineId, address, id);
+        Restaurant newRestaurant = new Restaurant(restaurantName, cuisineId, address, openTime, closeTime, id);
         allRestaurants.Add(newRestaurant);
       }
 
@@ -110,7 +129,7 @@ namespace BestRestaurants
       SqlConnection conn = DB.Connection();
       conn.Open();
 
-      SqlCommand cmd = new SqlCommand("INSERT INTO restaurants (restaurant_name, cuisine_id, address) OUTPUT INSERTED.id VALUES(@RestaurantName, @CuisineId, @Address);", conn);
+      SqlCommand cmd = new SqlCommand("INSERT INTO restaurants (restaurant_name, cuisine_id, address, open_time, close_time) OUTPUT INSERTED.id VALUES(@RestaurantName, @CuisineId, @Address, @OpenTime, @CloseTime);", conn);
 
       SqlParameter restaurantNameParameter = new SqlParameter();
       restaurantNameParameter.ParameterName = "@RestaurantName";
@@ -124,9 +143,19 @@ namespace BestRestaurants
       addressParameter.ParameterName = "@Address";
       addressParameter.Value = this.GetAddress();
 
+      SqlParameter openTimeParameter = new SqlParameter();
+      openTimeParameter.ParameterName = "@OpenTime";
+      openTimeParameter.Value = this.GetOpenTime();
+
+      SqlParameter closeTimeParameter = new SqlParameter();
+      closeTimeParameter.ParameterName = "@CloseTime";
+      closeTimeParameter.Value = this.GetCloseTime();
+
       cmd.Parameters.Add(restaurantNameParameter);
       cmd.Parameters.Add(cuisineIdParameter);
       cmd.Parameters.Add(addressParameter);
+      cmd.Parameters.Add(openTimeParameter);
+      cmd.Parameters.Add(closeTimeParameter);
 
       SqlDataReader rdr = cmd.ExecuteReader();
 
@@ -161,6 +190,8 @@ namespace BestRestaurants
       string foundRestaurantName = null;
       string foundAddress = null;
       int foundCuisineId = 0;
+      string foundOpenTime = null;
+      string foundCloseTime = null;
 
       while (rdr.Read())
       {
@@ -168,9 +199,11 @@ namespace BestRestaurants
         foundRestaurantName = rdr.GetString(1);
         foundCuisineId = rdr.GetInt32(2);
         foundAddress = rdr.GetString(3);
+        foundOpenTime = rdr.GetString(4);
+        foundCloseTime = rdr.GetString(5);
       }
 
-      Restaurant foundRestaurant = new Restaurant(foundRestaurantName, foundCuisineId, foundAddress, foundRestaurantId);
+      Restaurant foundRestaurant = new Restaurant(foundRestaurantName, foundCuisineId, foundAddress, foundOpenTime, foundCloseTime, foundRestaurantId);
 
       if (rdr != null)
       {
@@ -195,13 +228,13 @@ namespace BestRestaurants
       conn.Close();
     }
 
-    public void Update(string newRestaurantName = null, int newCuisineId = 0, string newAddress = null)
+    public void Update(string newRestaurantName = null, int newCuisineId = 0, string newAddress = null, string newOpenTime = null, string newCloseTime = null)
     {
       SqlConnection conn = DB.Connection();
       conn.Open();
 
       //new command to change any changed fields
-      SqlCommand cmd = new SqlCommand("UPDATE restaurants SET restaurant_name = @newRestaurantName, cuisine_id = @newCuisineId, address = @newAddress OUTPUT INSERTED.restaurant_name, INSERTED.cuisine_id, INSERTED.address WHERE id = @RestaurantId;", conn);
+      SqlCommand cmd = new SqlCommand("UPDATE restaurants SET restaurant_name = @newRestaurantName, cuisine_id = @newCuisineId, address = @newAddress, open_time = @openTime, close_time = @closeTime OUTPUT INSERTED.restaurant_name, INSERTED.cuisine_id, INSERTED.address, INSERTED.open_time, INSERTED.close_time WHERE id = @RestaurantId;", conn);
 
       //Get id of restaurant to use in command
       SqlParameter restaurantIdParameter = new SqlParameter();
@@ -257,6 +290,38 @@ namespace BestRestaurants
       }
       cmd.Parameters.Add(newAddressParameter);
 
+      //CHANGE OPEN TIME
+      SqlParameter newOpenTimeParameter = new SqlParameter();
+      newOpenTimeParameter.ParameterName = "@newOpenTime";
+
+      //If there is a new restaurant name, change it
+      if (!String.IsNullOrEmpty(newOpenTime))
+      {
+        newOpenTimeParameter.Value = newOpenTime;
+      }
+      //if there isn't a new restaurant name, don't change the name
+      else
+      {
+        newOpenTimeParameter.Value = this.GetOpenTime();
+      }
+      cmd.Parameters.Add(newOpenTimeParameter);
+
+      //CHANGE CLOSE TIME
+      SqlParameter newCloseTimeParameter = new SqlParameter();
+      newCloseTimeParameter.ParameterName = "@newCloseTime";
+
+      //If there is a new restaurant name, change it
+      if (!String.IsNullOrEmpty(newCloseTime))
+      {
+        newCloseTimeParameter.Value = newCloseTime;
+      }
+      //if there isn't a new restaurant name, don't change the name
+      else
+      {
+        newCloseTimeParameter.Value = this.GetCloseTime();
+      }
+      cmd.Parameters.Add(newCloseTimeParameter);
+
       //execute reader
       SqlDataReader rdr = cmd.ExecuteReader();
 
@@ -265,6 +330,8 @@ namespace BestRestaurants
         this._restaurantName = rdr.GetString(0);
         this._cuisineId = rdr.GetInt32(1);
         this._address = rdr.GetString(2);
+        this._openTime = rdr.GetString(3);
+        this._closeTime = rdr.GetString(4);
       }
       if(rdr!= null)
       {

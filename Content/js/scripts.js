c38cd4b47fd9e2c1ddfda7e5a0f68449b9f3cee8
Alexandra Holcombe and Renee Mei <alexandra+renee@>
2017-02-23 16:43:41 -0800

    Add any list scroller

---
+++ Content/js/scripts.js
@@ -0,0 +1,11 @@
+// $(document)(function(){
+//   $("#demo3").als(
+//     {
+//     visible_items: 5,
+//     scrolling_items: 2,
+//     orientation: "vertical",
+//     circular: "yes",
+//     autoscroll: "yes",
+//     interval: 2000
+//   });
+// })
